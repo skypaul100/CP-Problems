f9c15d999a877fe3840080223a67e2b9960b26d8
skypaul100 <61193125+skypaul100@users.noreply.github.com>
2023-05-24 11:19:44 +0530

    Update 02.MergeTwoSortedArrays(codingNinjas+Leetcode).cpp

--- Array/02.MergeTwoSortedArrays(codingNinjas+Leetcode).cpp
+++ Array/02.MergeTwoSortedArrays(codingNinjas+Leetcode).cpp
@@ -43,5 +43,53 @@ vector<int> ninjaAndSortedArrays(vector<int>& arr1, vector<int>& arr2, int m, in
 //Approach "2"
 //merge two sorted arrays into ""First"" array : coding ninjas + leetcode
 
+class Solution {
+public:
+void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+
+ int i = 0;
+
+  int j = 0;
+
+  //int k= m+n;
+
+  vector<int> nums3;
+
+  while (i < m && j < n) {
+
+    if (nums1[i] < nums2[j]) {
+
+      nums3.push_back(nums1[i++]);
+
+    } else {
+
+      nums3.push_back(nums2[j++]);
+
+    }
+
+  }
+
+  while (i < m) {
+
+    nums3.push_back(nums1[i++]);
+
+  }
+
+  while (j < n) {
+
+    nums3.push_back(nums2[j++]);
+
+  }
+
+//The final sorted array should not be returned by the function, but instead be 
+//stored inside the array nums1
+  nums1= nums3;
+  
+
+  //or use 'k' to put in nums1 using arrays
+
+ }
+};
+
 
 
