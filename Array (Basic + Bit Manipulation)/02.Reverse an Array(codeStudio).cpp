69e70ca1c22e36bb10139feb85d48ddd9037fcb7
skypaul100 <61193125+skypaul100@users.noreply.github.com>
2023-06-04 13:31:25 +0530

    Create 02.Reverse an Array(codeStudio).cpp

---
+++ Array (Basic + Bit Manipulation)/02.Reverse an Array(codeStudio).cpp
@@ -0,0 +1,28 @@
+
+//reverse an array 0 to n : codestudio
+void reverseArray(vector<int> &arr , int m)
+{
+	
+	int start= 0 , end=arr.size()-1;
+	while(start<=end)
+	{
+		swap(arr[start],arr[end]);
+		end--;
+		start++;
+	}
+}
+
+ 
+//reversethe array after 'm' indices m to n: codestudio
+void reverseArray(vector<int> &arr , int m)
+{
+	
+	int start=m+1,end=arr.size()-1;
+	while(start<=end)
+	{
+		swap(arr[start],arr[end]);
+		end--;
+		start++;
+	}
+}
+
