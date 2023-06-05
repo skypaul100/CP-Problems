2c641d7bd75533a425614c4108fd462dbab45a36
skypaul100 <61193125+skypaul100@users.noreply.github.com>
2023-06-05 12:03:32 +0530

    Create 04.Rotate Array by K pos.(leetcode-189).cpp

---
+++ Array/04.Rotate Array by K pos.(leetcode-189).cpp
@@ -0,0 +1,15 @@
+  void rotate(vector<int>& nums, int k) {
+
+        int n=nums.size();
+        vector<int> temp(nums.size());
+
+        for(int i=0 ; i<n ; i++)
+        {
+          temp[(i+k)%n]=nums[i];
+        }
+        nums=temp;
+    
+        for(int i=0 ; i<n ; i++)
+           cout<<nums[i]<<" ";
+        
+    }
