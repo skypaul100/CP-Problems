3fa12a3e87f4c69c58c5ca3263df8dfdcd8a8a7c
skypaul100 <61193125+skypaul100@users.noreply.github.com>
2023-06-18 15:54:59 +0530

    Update 02.Reverse a String.cpp

--- Strings/02.Reverse a String.cpp
+++ Strings/02.Reverse a String.cpp
@@ -1,5 +1,33 @@
-string reverse(char str[])
-{
+
+#include <iostream>
+using namespace std;
+//Func. to get length of string
+int getLen(char str[]){
+    int count=0;
+    for(int i=0; str[i]!='\0'; i++)
+    {
+        count++;
+    }
+    return count;
+}
+//Func to reverse a string
+string reverse(char str[],int n){
+   int s=0;
+   int e=n-1;
+   while(s<e){
+       swap(str[s++],str[e--]);
+   }
+   return str;
+}
+int main() {
+    char str[20];
+    cout<<"Enter a String: ";
+    cin.getline(str,20);
     
-    return str;
+    int len=getLen(str);
+    
+    cout<<"Reverse of String: ";
+    cout<<reverse(str,len);
+    
+    return 0;
 }
