116420568bbc41c971ae41e2106a0f020b4145bf
skypaul100 <61193125+skypaul100@users.noreply.github.com>
2023-06-18 16:19:12 +0530

    Update 03.Check isPalindrome(codeStudio).cpp

--- Strings/03.Check isPalindrome(codeStudio).cpp
+++ Strings/03.Check isPalindrome(codeStudio).cpp
@@ -1,7 +1,29 @@
 //codeStudio: check if string is Palindrome
 
 
-/*****Basic Type: Full Palindrome Solution******/
+/***** Full Palindrome Solution******/
+//Basic Type: checking 1st and last elt, 2nd and 2nd last and so on
+
+#include <iostream>
+using namespace std;
+int getLen(char str[]){
+    int count=0;
+    for(int i=0; str[i]!='\0'; i++)
+    {
+        count++;
+    }
+    return count;
+}
+//Func to reverse a string
+string reverse(char str[],int n){
+   int s=0;
+   int e=n-1;
+   while(s<e){
+       swap(str[s++],str[e--]);
+   }
+   return str;
+}
+//checks palindrome
 bool checkPalindrome(char str[],int n)
 {
   int s=0 ;
@@ -9,7 +31,7 @@ bool checkPalindrome(char str[],int n)
   
   while(s<=e)
   {
-    if(a[s]!=a[e]) // Not Matches
+    if(str[s]!=str[e]) // Not Matches
     {
       return 0;
     }
@@ -19,3 +41,23 @@ bool checkPalindrome(char str[],int n)
     }
     return 1;
   }
+}
+int main() {
+    char str[20];
+    cout<<"Enter a String: ";
+    cin.getline(str,20);
+    
+    int len=getLen(str);
+    
+    cout<<"Reverse of String: ";
+    cout<<reverse(str,len);
+    
+    cout<<endl;
+    cout<<"Palindrome: ";
+    if(checkPalindrome(str,len)==1)
+    cout<<"Yes";
+    else
+     cout<<"No";
+      
+    return 0;
+}
