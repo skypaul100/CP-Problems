3dd38660e7040002e9172f59ed3f7000e1055454
skypaul100 <61193125+skypaul100@users.noreply.github.com>
2023-07-01 17:27:58 +0530

    Create 05.Print names of digits name.cpp

---
+++ Recursion/05.Print names of digits name(extra).cpp
@@ -0,0 +1,40 @@
+//Interview Question
+//Time= O(n)
+//Space= O(n)
+
+#include<iostream> 
+using namespace std;
+//Using Recursion
+void sayDigit(int n, string arr[]) {
+
+    //base case
+    if(n == 0)
+        return ;
+
+    //processing 
+    int digit = n % 10;
+    n = n / 10;
+    
+
+    //recursive call
+    sayDigit(n, arr);
+
+    cout << arr[digit] << " ";
+
+}
+
+int main() {
+
+    string arr[10] = {"zero", "one", "two", "three",
+                         "four", "five", "six"
+                        , "seven", "eight", "nine"};
+    int n;
+    cin >> n;
+
+    cout << endl ;
+    sayDigit(n, arr); //Function call
+    cout << endl  ;
+
+
+    return 0;
+}
