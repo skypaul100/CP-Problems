a02e432e0a8673738b425af51a042987a8f3e312
skypaul100 <61193125+skypaul100@users.noreply.github.com>
2023-07-06 15:12:33 +0530

    Create 18.Add Plus 1 to last digit in array(leetcode66).cpp

---
+++ Array/18.Add Plus 1 to last digit in array(leetcode66).cpp
@@ -0,0 +1,30 @@
+//leetcode(easy): 66.Plus One
+//Time: O(n)
+//Space: O(1)
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+
+        int n=digits.size();
+        for(int i=n-1 ; i>=0 ; i--)
+        {
+            if(i==n-1)
+              digits[i]++;
+            if(digits[i]==10)
+            {
+                digits[i]=0;
+
+            if(i!=0)
+                digits[i-1]++;
+
+            else 
+            {
+                digits.push_back(0);
+                digits[i]++;
+            }
+         }
+        }
+        return digits;
+    }
+};
